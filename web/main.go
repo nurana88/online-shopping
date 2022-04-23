a04134ebcdd787736f2b30d17dc1806da220abf1
‘nurana88’ <nurana.mirzaghayeva@gmail.com>
2022-04-23 18:34:17 +0200

    Updated

--- web/main.go
+++ web/main.go
@@ -4,19 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/nurana88/shop-template/pkg/handlers"
 )
 
 func main() {
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/shop", handlers.Shop)
+	// http.HandleFunc("/", handlers.Home)
+	// http.HandleFunc("/shop", handlers.Shop)
+
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("../templates"))))
 
 	fmt.Println("Starting session on :9000...")
 	log.Fatal(http.ListenAndServe(":9000", nil))
-
-	// http.Handle("/temp/", http.StripPrefix("/temp", http.FileServer(http.Dir("../templates"))))
 
 	// log.Fatal(http.ListenAndServe(":9000", http.FileServer(http.Dir("../templates"))))
 }
