16e9642963a02d3d7d9a539dbecf1094be409f3b
Nurana Mirzaghayeva <nurana.mirzaghayeva@gmail.com>
2022-10-31 12:05:12 +0100

    Update main file

--- main.go
+++ main.go
@@ -1,47 +1,42 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
+	"github.com/nurana88/online-shopping/pkg/command"
+	"github.com/urfave/cli"
 	"log"
-	"net/http"
-
-	"github.com/nurana88/online-shopping/Infrastructure"
-	"github.com/nurana88/online-shopping/Presentation/HTTP/Handlers"
-	domain "github.com/nurana88/online-shopping/domain"
-	"github.com/nurana88/online-shopping/pkg/handlers"
+	"os"
 )
+
+// Build version. Sent as a linker flag in Makefile
+var version string
 
 func main() {
 
-	dataSourceName := "root:pwd@tcp(127.0.0.1:3306)/testdb"
+	fmt.Println("In the main")
+	version = "1"
+	app := cli.NewApp()
+	app.Version = version
+	app.Name = "Online shopping platform"
+	app.Usage = "App provides online shopping and makes life easier for lovely customers ;)"
 
-	var err error
-	DB, err := sql.Open("mysql", dataSourceName)
-	if err != nil {
-		fmt.Println("Couldn't connect to DB", err)
-		return
+	basecommand := command.NewBaseCommand(context.TODO())
+	httpCommand := command.NewHTTPCommand(basecommand)
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "http",
+			Usage:  "Registers customer",
+			Action: httpCommand.Run,
+		},
 	}
 
-	err = DB.Ping()
+	err := app.Run(os.Args)
 	if err != nil {
-		fmt.Println("Can not be connected in Ping", err)
-		return
+		log.Fatalln(err)
 	}
-	fmt.Println("Successfully connected to DB...")
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/register", handlers.Register)
-	http.HandleFunc("/registerauth", Handlers.NewRegisterUserHandler(domain.NewDbUserUsercase(Infrastructure.NewDbActions(DB))))
-	http.HandleFunc("/login", handlers.Login)
-	http.HandleFunc("/loginauth", Handlers.LoginUser(domain.NewDbUserUsercase(Infrastructure.NewDbActions(DB))))
+	fmt.Println("At the end main")
 
-	http.HandleFunc("/welcome", handlers.Welcome)
-
-	// http.Handle("/assets", http.StripPrefix("/assets", http.FileServer(http.Dir("assets/"))))
-
-	fmt.Println("Starting session on :9000...")
-	log.Fatal(http.ListenAndServe(":9000", nil))
-
-	// log.Fatal(http.ListenAndServe(":9000", http.FileServer(http.Dir("../templates"))))
 }
